8a5a78a24d547e1c395fb1a7310d79c867311cb7
Denis Vieira <denis.vieira@softvaro.com.br>
2019-09-22 21:06:40 -0300

    added first vercion
    Signed-off-by: Denis Vieira <denis.vieira@softvaro.com.br>

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#FFF',
+
+  primary: '#000000',
+  primaryLight: '#000',
+  primaryDark: '#000',
+
+  secondary: '#ccc',
+  secondaryLight: '#000',
+  secondaryDark: '#000',
+};
