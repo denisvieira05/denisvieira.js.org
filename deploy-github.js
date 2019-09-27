80fb7ba70d366778a46eae431a3166930b224047
Denis Vieira <denisvieira05@gmail.com>
2019-09-27 00:40:55 -0300

    fix master
    Signed-off-by: Denis Vieira <denisvieira05@gmail.com>

--- deploy-github.js
+++ deploy-github.js
@@ -4,7 +4,7 @@ const ghpages = require('gh-pages');
 ghpages.publish(
   'public',
   {
-    branch: 'master',
+    branch: 'gh-pages',
     repo: 'https://github.com/denisvieira05/denisvieira05.github.io',
   },
   () => {
